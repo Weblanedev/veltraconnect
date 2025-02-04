44619be0c9029441b7c4c3b0ef1cdde1671c5741
Bunmi Oye <bunmioye09@gmail.com>
2025-02-04 18:16:41 +0100

    add logo to preloader

--- src/components/common/Preloader.tsx
+++ src/components/common/Preloader.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import logoImg from '../../../public/assets/img/logo/logo.svg';
+// import logoImg from '../../../public/assets/img/logo/logo.svg';
 import preloaderIMg from '../../../public/assets/img/logo/preloader.svg';
 import Image from 'next/image';
+import LogoText from './logo-text/LogoText';
 
 const Preloader = () => {
   return (
@@ -10,7 +11,8 @@ const Preloader = () => {
         <div id="loading-center">
           <div id="loading-center-absolute">
             <div className="loading-icon text-center d-flex flex-column align-items-center justify-content-center">
-              <Image src={logoImg} alt="logo-img" />
+              {/* <Image src={logoImg} alt="logo-img" /> */}
+              <LogoText />
               <Image className="loading-logo" src={preloaderIMg} alt="img" />
             </div>
           </div>
